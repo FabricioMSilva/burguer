51c9bebdca140ae13d56f6594b56b62707cf6bba
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2021-10-11 22:22:27 -0300

    Update footer.jsx

--- src/componentes/footer.jsx
+++ src/componentes/footer.jsx
@@ -14,20 +14,20 @@ return (
             <div class="container">
                 <div class="row">
                     <div class="col-sm-4 col-md-2 item text-center">
-                        <img className="img-responsive" width="100" height="100" src={Ernane}></img>
+                        <img className="img-responsive" width="100" alt="ernanefoto" height="100" src={Ernane}></img>
                         <h2>Ernane Mendes</h2>
                         <h3>Proprietário</h3>
                     </div>
                     <div class="col-sm-6 col-md-3 item">
                         <h3>E-Burguer</h3>
                         <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Planejamento</a></li>
-                            <li><a href="#">Logistica</a></li>
-                            <li><a href="#">Marketing</a></li>
-                            <li><a href="#">Equipamentos</a></li>
-                            <li><a href="#">Ingradientes</a></li>
-                            <li><a href="#">Acompanhamentos</a></li>
+                            <li><a>Home</a></li>
+                            <li><a>Planejamento</a></li>
+                            <li><a>Logistica</a></li>
+                            <li><a>Marketing</a></li>
+                            <li><a>Equipamentos</a></li>
+                            <li><a>Ingradientes</a></li>
+                            <li><a>Acompanhamentos</a></li>
                         </ul>
                     </div>
                     <div class="col-md-6 item text">
@@ -39,9 +39,9 @@ return (
                             ⏰Terça a Domingo de 19h às 23h<br/>
                             👉🏼Recuse Imitações<br/>
                             📱24 98845-2525 </p>
-                            <a href="https://eburgersbm.grandchef.com.br/"target="_blank">Site para pedidos E-Burguer</a>
+                            <a href="https://eburgersbm.grandchef.com.br/"target="parent" rel="noopener" >Site para pedidos E-Burguer</a>
                     </div>
-                    <div class=""><a href="https://www.facebook.com/E-Burgers-105759164386676" target="_blank"><i class=" fab fa-facebook-square fa-3x fa-bar"></i></a><br/><a href="https://www.instagram.com/e_burgers_/" target="_blank"><i class="fab fa-instagram fa-3x fa-bar"></i></a></div>
+                    <div class=""><a href="https://www.facebook.com/E-Burgers-105759164386676" target="parent" rel="noopener"><i class=" fab fa-facebook-square fa-3x fa-bar"></i></a><br/><a href="https://www.instagram.com/e_burgers_/" target="parent" rel="noopener"><i class="fab fa-instagram fa-3x fa-bar"></i></a></div>
                 </div>
                 <p class="copyright">E-Burguer © 2020</p>
             </div>
